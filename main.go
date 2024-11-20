d0583eba4dabbe653b4245ae606add263b134bb9
Srikanth K <kolleparasrikanth@gmail.com>
2024-11-20 21:10:54 +0530

    structs as receiver function example

--- main.go
+++ main.go
@@ -14,24 +14,30 @@ type contractInfo struct {
 }
 
 func main() {
-	sri := person{firstName: "Sri", lastName: "K"}
-	fmt.Println(sri)
-	//diff way to define struct
-	var ram person
-	ram.firstName = "Ram"
-	ram.lastName = "R"
-	fmt.Println(ram)
-	fmt.Printf("%+v", ram)
-	//diff way
-	Krish := person{"Krish", "K", contractInfo{"sri@sri.com", 1234}}
-	fmt.Println(Krish)
+	// sri := person{firstName: "Sri", lastName: "K"}
+	// fmt.Println(sri)
+	// //diff way to define struct
+	// var ram person
+	// ram.firstName = "Ram"
+	// ram.lastName = "R"
+	// fmt.Println(ram)
+	// fmt.Printf("%+v", ram)
+	// //diff way
+	// Krish := person{"Krish", "K", contractInfo{"sri@sri.com", 1234}}
+	// fmt.Println(Krish)
 
-	// Krish := person{
-	// 	"Krish",
-	// 	"K",
-	// 	contractInfo{
-	// 		"sri@sri",
-	// 		1234,
-	// 	},
-	// }
+	Krish := person{
+		"Krish",
+		"K",
+		contractInfo{
+			"sri@sri",
+			1234,
+		},
+	}
+
+	Krish.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
