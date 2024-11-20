cb5c5abd8011e3cde56fa40f6bbf10d40ecf2cde
Srikanth K <kolleparasrikanth@gmail.com>
2024-11-20 20:28:43 +0530

    diff ways to initiate Struct

--- main.go
+++ main.go
@@ -10,4 +10,14 @@ type person struct {
 func main() {
 	sri := person{firstName: "Sri", lastName: "K"}
 	fmt.Println(sri)
+	//diff way to define struct
+	var ram person
+	ram.firstName = "Ram"
+	ram.lastName = "R"
+	fmt.Println(ram)
+	fmt.Printf("%+v", ram)
+	//diff way
+
+	Krish := person{"Krish", "K"}
+	fmt.Println(Krish)
 }
