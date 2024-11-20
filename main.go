3b2950391e5863465c78aa06414dd3ecf9439993
Srikanth K <kolleparasrikanth@gmail.com>
2024-11-20 20:20:40 +0530

    using of a declared Struct

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
-
+	sri := person{firstName: "Sri", lastName: "K"}
+	fmt.Println(sri)
 }
